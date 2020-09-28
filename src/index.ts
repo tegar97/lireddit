6f38f9cae8e33bbfc4cc16aa7f11e00c51ddd681
Muhammad Tegar Akmal <eategar@gmail.com>
2020-09-28 13:36:10 +0700

    This is a new commit for what I originally planned to be amended

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,34 @@
+import 'reflect-metadata'
 import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
-import { Post } from "./entities/Post";
+// import { Post } from "./entities/Post";
 import microConfig from "./mikro-orm.config";
-
+import express from 'express'
+import {ApolloServer} from 'apollo-server-express'
+import {buildSchema} from 'type-graphql'
+import {HelloResolver} from './resolvers/helo'
+import {PostResolver} from './resolvers/post'
 const main = async () => {
   const orm = await MikroORM.init(microConfig);
   await orm.getMigrator().up();
-  const post = orm.em.create(Post, { title: "my first post" });
-  await orm.em.persistAndFlush(post);
+  // const post = orm.em.create(Post, { title: "my first post" });
+  // await orm.em.persistAndFlush(post);
 
-  const posts = await orm.em.find(Post, {});
-  console.log(posts);
+  // const Posts = await orm.em.find(Post, {});
+  // console.log(Posts);
+  const app = express();
+
+  const appoloServer = new ApolloServer({
+    schema : await buildSchema({
+      resolvers: [HelloResolver,PostResolver],
+      validate: false
+    }),
+    context: () => ({em: orm.em})
+  })
+  appoloServer.applyMiddleware({app})
+  app.listen(4000,() => {
+    console.log('server started on localhost:4000')
+  })
 };
 
 main().catch((err) => {
--- src/migrations/Migration20200928032456.ts
+++ src/migrations/Migration20200928032456.ts
@@ -1,9 +1,11 @@
-import { Migration } from "@mikro-orm/migrations";
+import { Migration } from '@mikro-orm/migrations';
 
-export class Migration20200808202509 extends Migration {
+export class Migration20200928032456 extends Migration {
+
   async up(): Promise<void> {
     this.addSql(
       'create table "post" ("id" serial primary key, "created_at" timestamptz(0) not null, "updated_at" timestamptz(0) not null, "title" text not null);'
     );
   }
+
 }
